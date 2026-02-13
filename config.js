f3ed42645c3f41c1f424b32a2652329d885f694f
invictus008 <thamizhan0812@gmail.com>
2026-02-13 10:56:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug,a huge kiss and your flowers! <a href='YOUR_DIGITAL_BOUQUET_URL_HERE' target='_blank'>Get your flowers here 💐</a>",
+        message: "Now come get your gift, a big warm hug,a huge kiss and your flowers! <a href='https://digibouquet.vercel.app/bouquet/40b57c4c-aa05-4f5d-8c4b-31bc126ccc05' target='_blank'>Get your flowers here 💐</a>",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
